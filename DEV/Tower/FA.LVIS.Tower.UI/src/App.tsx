df6c918b7b5e51d27f39269d6a095727fb56897e
None <None>
2024-03-13 10:01:35 +0100

    rename_file

--- DEV/Tower/FA.LVIS.Tower.UI/src/App.tsx
+++ DEV/Tower/FA.LVIS.Tower.UI/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        {/* Your Navbar or header content here */}
       </header>
+      <main>
+        {/* Main content goes here */}
+      </main>
     </div>
   );
-}
-
+};
 export default App;
