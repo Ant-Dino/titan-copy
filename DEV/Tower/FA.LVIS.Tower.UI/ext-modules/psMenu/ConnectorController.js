2de1f189d0b9022e4e0449d8dc8467a78c63ab21
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-20 13:20:28 +0100

    edit_file

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMenu/ConnectorController.js
+++ DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psMenu/ConnectorController.js
@@ -1,42 +1,41 @@
-﻿"use strict";
-
-angular.module('psMenu').controller('ConnectorController',
-    ['$scope', '$http', '$interval', 'growl', 'GetApplicationStatus',
-        function ($scope, $http, $interval, growl, GetApplicationStatus) {
-            var Connector = this;
-            //$http.get('ApplicationController/GetApplicationStatus/')
-            // .then(function (response) {
-            //     Connector.BiztalkPortList = response.data;
-            // }, function (data) {
-            //     growl.error("Unable to retrieve application information at this time.");
-            // });
-
-            //Connector.LoadApplicationStatus = function () {
-            //    var datapromise = GetApplicationStatus.getApplicationStatus();
-            //    datapromise.then(function (data) {
-            //        if (data.length != 0) {
-            //            Connector.BiztalkPortList = data;
-            //            var Count = 0;
-            //            //collect array object Properties
-            //            angular.forEach(Connector.BiztalkPortList, function (item) {
-            //                if (!item.Active) {
-            //                    Count = Count + 1;
-            //                }
-            //                $rootScope.ApplicationStatusCount = Count;
-            //            });
-
-            //            return;
-            //        }
-            //        else {
-            //            growl.error("Unable to retrieve application information at this time.");
-            //        }
-            //    });
-            //};
-
-            //Connector.LoadApplicationStatus();
-
-            //$interval(function () {
-            //    Connector.LoadApplicationStatus();
-            //}.bind(this), 30000);
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+const ConnectorComponent = () => {
+    const [biztalkPortList, setBiztalkPortList] = useState([]);
+    const [applicationStatusCount, setApplicationStatusCount] = useState(0);
+    const loadApplicationStatus = async () => {
+        try {
+            const response = await axios.get('ApplicationController/GetApplicationStatus/');
+            const data = response.data;
+            if (data.length !== 0) {
+                setBiztalkPortList(data);
+                let count = 0;
+                data.forEach((item) => {
+                    if (!item.Active) {
+                        count = count + 1;
+                    }
+                });
+                setApplicationStatusCount(count);
+            } else {
+                toast.error("Unable to retrieve application information at this time.");
+            }
+        } catch (error) {
+            toast.error("Unable to retrieve application information at this time.");
         }
-    ]);
+    };
+    useEffect(() => {
+        loadApplicationStatus();
+        const intervalId = setInterval(() => {
+            loadApplicationStatus();
+        }, 30000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+    return (
+        <div>
+            {/* Render your component UI based on biztalkPortList and applicationStatusCount */}
+        </div>
+    );
+};
+export default ConnectorComponent;
