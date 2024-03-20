b68ac56dac272827fe2c3302f85bc75c3626da29
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-03-20 13:20:47 +0100

    edit_file

--- DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psUtilities/psEndpointAccess/psEndPointAccessController.js
+++ DEV/Tower/FA.LVIS.Tower.UI/ext-modules/psUtilities/psEndpointAccess/psEndPointAccessController.js
@@ -1,58 +1,70 @@
-﻿"use strict";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Modal, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { toast } from 'react-toastify';
+const PsEndPointAccessComponent = () => {
+  const history = useHistory();
+  const [cookies, setCookie] = useCookies(['activityright']);
+  const [activityRight, setActivityRight] = useState(cookies.activityright || '');
+  const [tenantName, setTenantName] = useState('');
+  const [applicationList, setApplicationList] = useState([]);
+  const [showMessage, setShowMessage] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  useEffect(() => {
+    // Replace the event emitter with React context or props,
+    // depending on how getUser is triggered in your application.
 
-angular.module('psEndpointAccess').controller('psEndPointAccessController', psEndPointAccessController);
+    // Mockup for demonstration
+    const { activityRight: newActivityRight, tenantname: newTenantName } = { activityRight: 'SuperAdmin', tenantname: 'LVIS' };
+    setActivityRight(newActivityRight);
+    setTenantName(newTenantName);
 
-psEndPointAccessController.$inject = ['$route', '$routeParams', '$scope', '$rootScope', '$http', '$uibModal', '$location', '$cookies', 'growl',];
-function psEndPointAccessController($route, $routeParams, $scope, $rootScope, $http, $uibModal, $location, $cookies, growl) {
-
-    var vmUtilitiesEndPoint = this;
-    $scope.showMessage = false;
-
-    $scope.$on("getUser", function (evt, response) {
-        $rootScope.activityright = response.ActivityRight;
-        $rootScope.tenantname = response.tenantname;
-    });
-
-    if (!$rootScope.activityright) {
-        $rootScope.activityright = $cookies.get('activityright');
+    if (activityRight !== 'SuperAdmin' || tenantName !== 'LVIS') {
+      alert('You are not authorized to view this page.');
+      history.push('/dashboard');
     }
 
-    if ($rootScope.activityright !== 'SuperAdmin' || $rootScope.tenantname !== 'LVIS') {
-        var instance = $uibModal.open({
-            template: '<div class="widget-header"><i class="fa fa-lg fa-exclamation-triangle"></i> <h3>Attention</h3></div><div class="modal-body">You are not authorized to view this page.</div>' +
-                '<div class="modal-footer"><a class="btn btn-default" ng-click="$close()">Close</a></div>', size: 'sm'
-        });
-
-        instance.result.finally(function () {
-            $location.path('/dashboard');
-        });
-    }
-
-    $http.get('UtilitiesController/GetEndPointApplications')
-        .then(function (response) {
-            vmUtilitiesEndPoint.ApplicationList = response.data;
-        });
-
-    vmUtilitiesEndPoint.params = {};
-    $scope.showPassword = false;
-    vmUtilitiesEndPoint.toggleShowPassword = function () {
-        $scope.showPassword = !$scope.showPassword;
-    }
-      vmUtilitiesEndPoint.save =
-          function save() {
-              vmUtilitiesEndPoint.Submitted = true;
-            $http.post('UtilitiesController/AddCredentials/', vmUtilitiesEndPoint)
-                .then(function (response) {
-                    if (response.data == true) {
-                        $scope.showMessage = true;
-                        growl.success("Credentials for Application" + " was updated successfully ");
-                        vmUtilitiesEndPoint.Submitted = false;
-                    }
-                },
-                    function (error) {
-                        $scope.showMessage = true;
-                        growl.error("There was an error in Inserting the credentials.");
-                        vmUtilitiesEndPoint.Submitted = false;
-                    });
+    axios.get('UtilitiesController/GetEndPointApplications')
+      .then((response) => {
+        setApplicationList(response.data);
+      });
+  }, [history, activityRight, tenantName]);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const save = () => {
+    setSubmitted(true);
+    axios.post('UtilitiesController/AddCredentials/', { /* data */ })
+      .then((response) => {
+        if (response.data === true) {
+          setShowMessage(true);
+          toast.success("Credentials for Application was updated successfully");
+          setSubmitted(false);
         }
-}
+      })
+      .catch((error) => {
+        setShowMessage(true);
+        toast.error("There was an error in Inserting the credentials.");
+        setSubmitted(false);
+      });
+  };
+  return (
+    <div>
+      {showMessage && (
+        <div>
+          Message here
+        </div>
+      )}
+      <button onClick={toggleShowPassword}>
+        {showPassword ? 'Hide Password' : 'Show Password'}
+      </button>
+      <button onClick={save} disabled={submitted}>
+        Save
+      </button>
+    </div>
+  );
+};
+export default PsEndPointAccessComponent;
